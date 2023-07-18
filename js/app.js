0eb70929acf858aa7b84f0e08f6d0227a82f1b62
alexcodaline <aleksander.ponom@gmail.com>
2023-07-18 11:45:45 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// завдання №1
+let a = prompt("Enter number a:");
+let b = prompt("Enter number b:");
+function maxNumber(a, b) {
+  if (Number(a) > Number(b)) {
+    return "number a";
+  } else if (Number(b) > Number(a)) {
+    return "number b";
+  } else {
+    return "number a=b";
+  }
+}
+alert(maxNumber(a, b));
+// завдання №2
+// let number = prompt('Enter the number');
+// function revers(number) {
+//   number = Number(number);
+//   return -number;
+// }
+// alert(revers(number));
+// завдання №3
+// let number = prompt('Enter number');
+// let count = prompt('Enter count');
+// function three(number, count) {
+//   return (Number(number + 3 * count));
+// }
+// alert(three(+number, +count));
+// завдання №4
+// let km = prompt("Enter kilometrs:");
+// let metric = prompt("Enter: m or cm");
+// function KmToM(km) {
+//   return km * 1000;
+// }
+// function KmToCm(km) {
+//   return km * 100000;
+// }
+// function getMetric(metric, km, m, cm) {
+//   if (metric === "m") {
+//     return KmToM(km);
+//   }
+//   if (metric === "cm") {
+//     return KmToCm(km);
+//   }
+// }
+// alert(getMetric(metric, km, KmToM, KmToCm));
