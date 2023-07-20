10117685df8f66ec9c0b66325105808bc4342082
alexcodaline <aleksander.ponom@gmail.com>
2023-07-20 13:59:11 +0300

    №1

---
+++ lesson22/js/app.js
@@ -0,0 +1,12 @@
+// завдання 1
+let car = {
+  model: "Sportage",
+  year: "2018",
+  color: "black",
+  signal() {
+    alert("fa! fa!");
+  },
+};
+car.color = "white";
+car.type = "electric";
+// завдання 2
