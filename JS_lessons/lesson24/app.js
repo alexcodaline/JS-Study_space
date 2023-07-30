47831b90d47fd8e27c460fd2ec06a9e3a5fa29d1
alexcodaline <aleksander.ponom@gmail.com>
2023-07-30 16:35:50 +0300

    task 1

---
+++ JS_lessons/lesson24/app.js
@@ -0,0 +1,22 @@
+// завдання 1
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function sumArr(arr, item = 0) {
+//   if (item === arr.length) {
+//     return 0;
+//   } else {
+//     return arr[item] + sumArr(arr, item + 1);
+//   }
+// }
+// let summa = sumArr(arr);
+// console.log(summa);
+
+// скорочений запис
+
+// function sumArr(arr, item = 0) {
+//   return item === arr.length ? 0 : arr[item] + sumArr(arr, item + 1);
+// }
+// let summa = sumArr(arr);
+// console.log(summa);
+
+//  завдання 2
